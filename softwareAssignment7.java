f2caf93563e32581a7d495f94c88456cca29e839
adarablichfeldt <77905333+adarablichfeldt@users.noreply.github.com>
2021-10-16 17:22:58 -0400

    Add files via upload

---
+++ softwareAssignment7.java
@@ -0,0 +1,11 @@
+
+public class softwareAssignment7 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 4;
+		int sum = a + b;
+		System.out.println("The sum of " + a + " + " + b + " = " + sum);
+	}
+
+}
